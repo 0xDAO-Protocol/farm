57be1360209cfc75447ec90b630b348be8d15c3b
0xBebis <bytemasons@protonmail.com>
2022-01-26 16:43:29 -0800

    pushing Emission Killer changes

--- contracts/OXDBurner.sol
+++ contracts/OXDBurner.sol
@@ -8,35 +8,30 @@ import './utils/Ownable.sol';
 import './interfaces/IMasterChef.sol';
 
 
-contract EmissionKiller is IERC20, ERC20 {
+contract EmissionKiller is IERC20, ERC20, Ownable {
     using SafeERC20 for IERC20;
 
     uint public immutable pid;
     IMasterChef public constant chef = IMasterChef(0xa7821C3e9fC1bF961e280510c471031120716c3d);
     IERC20 public constant OXD = IERC20(0xc165d941481e68696f43EE6E99BFB2B23E0E3114);
-    address public constant multisig = 0xa96D2F0978E317e7a97aDFf7b5A76F4600916021;
-
 
     constructor (string memory name, string memory symbol, uint _pid) ERC20 (name, symbol) {
         _mint(address(this), 1);
         pid = _pid;
     }
 
-    function kill() external returns (bool) {
-        onlyOwner();
-        approve(address(chef), 1);
+    function kill() external onlyOwner returns (bool) {
+        _approve(address(this), address(chef), 1);
         chef.deposit(pid, 1);
         return true;
     }
 
-    function claimOXD() public returns (bool) {
-        onlyOwner();
+    function claimOXD() public onlyOwner returns (bool) {
         chef.deposit(pid, 0);
         return true;
     }
 
-    function destroy(uint amount, bool claim) public returns (bool) {
-        onlyOwner();
+    function destroy(uint amount, bool claim) public onlyOwner returns (bool) {
         if (claim) {
           claimOXD();
         }
@@ -44,8 +39,7 @@ contract EmissionKiller is IERC20, ERC20 {
         return true;
     }
 
-    function destroyAll(bool claim) external returns (bool) {
-        onlyOwner();
+    function destroyAll(bool claim) external onlyOwner returns (bool) {
         if (claim) {
           claimOXD();
         }
@@ -53,8 +47,7 @@ contract EmissionKiller is IERC20, ERC20 {
         return true;
     }
 
-    function divert(uint amount, address target, bool claim) public returns (bool) {
-        onlyOwner();
+    function divert(uint amount, address target, bool claim) public onlyOwner returns (bool) {
         if (claim) {
           claimOXD();
         }
@@ -62,17 +55,11 @@ contract EmissionKiller is IERC20, ERC20 {
         return true;
     }
 
-    function divertAll(address target, bool claim) external returns (bool) {
-        onlyOwner();
+    function divertAll(address target, bool claim) external onlyOwner returns (bool) {
         if (claim) {
           claimOXD();
         }
         divert(OXD.balanceOf(address(this)), target, false);
         return true;
     }
-
-    function onlyOwner() private view {
-        require(msg.sender == multisig, "not owner");
-    }
-
 }
